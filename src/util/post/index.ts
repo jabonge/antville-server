96fe031578683e7ba34db3f982b228b2072985af
jaebong <yuwoqhd@gmail.com>
2021-09-08 20:51:30 +0900

    cashTag one length

--- src/util/post/index.ts
+++ src/util/post/index.ts
@@ -5,7 +5,7 @@ import ogs, {
 } from 'open-graph-scraper';
 
 export function findCacheTags(str: string): string[] {
-  const cacheTagRegex = /\$([a-zA-Z가-힣-&0-9]{2,30})/g;
+  const cacheTagRegex = /\$([a-zA-Z가-힣-&0-9]{1,30})/g;
   const matches = [];
   let match;
   while ((match = cacheTagRegex.exec(str))) {
