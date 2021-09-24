abc7e53dfbf6916a9b73a59fc7f484d739581010
jaebong <yuwoqhd@gmail.com>
2021-09-24 16:49:02 +0900

    comment,post body length extend

--- src/domain/post/dto/create-post.dto.ts
+++ src/domain/post/dto/create-post.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, MaxLength, IsEnum, IsOptional } from 'class-validator';
 import { Sentiment } from '../entities/post.entity';
 
 export class CreatePostDto {
-  @MaxLength(1000)
+  @MaxLength(1200)
   @IsNotEmpty()
   body: string;
   @IsOptional()
--- src/domain/post/entities/post.entity.ts
+++ src/domain/post/entities/post.entity.ts
@@ -31,7 +31,7 @@ export class Post {
   id: number;
 
   @Column({
-    length: 1000,
+    length: 1500,
   })
   body: string;
 
